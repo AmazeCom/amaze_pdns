00750223b537ff2bad237f9619f1f9e02f3da839
root <root@test-dns.vm>
2019-11-12 09:29:54 +1100

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Amaze Communication'
+maintainer_email 'admin@amaze.com.au'
+license 'apachev2'
+version '0.1.0'
+chef_version '>= 13.0'
+
+depends 'delivery-truck'
